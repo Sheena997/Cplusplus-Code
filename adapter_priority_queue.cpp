ebd56e6d061ebc5c292824d851ef88f4bd47955e
Sheena爽 <48825486+Sheena997@users.noreply.github.com>
2020-05-11 13:59:47 +0800

    施爽：于2019年11月5日上传的用适配器实现优先级队列的代码。
    施爽：于2019年11月5日上传的用适配器实现优先级队列的代码。

--- adapter_priority_queue.cpp
+++ adapter_priority_queue.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <functional> //greater的头文件
 
 using namespace std;
-
 namespace sheena
 {
-	template<class T>
-	class my_less
+	template <class T>
+	struct my_less
 	{
-	public:
-		bool operator()(const T& a, const T& b)
+		//构造大根堆
+		bool operator()(const T& l, const T& r)
 		{
-			return a < b;
+			return l < r;
 		}
 	};
-	template<class T>
+
+	template <class T>
 	class my_greater
 	{
 	public:
-		bool operator()(const T&a, const T& b)
+		//构造小根堆
+		bool operator()(const T& l, const T& r)
 		{
-			return  a > b;
+			return l > r;
 		}
 	};
 
-
-    template<class T, class Contanier = vector<T>, class compare = my_less<T>>
-	class priority_queue
+	template <class T, class Container = vector<T>, class Compare = my_less<T>>
+	class my_priority_queue
 	{
 	public:
+		
 		void adjust_up(int child)
 		{
-			compare com;
+			Compare com;
 			int parent = (child - 1) / 2;
 			while (child > 0)
 			{
@@ -46,35 +49,33 @@ namespace sheena
 					break;
 				}
 			}
-
 		}
-		void adjust_down(int root)
-		{
-			compare com;
-			int parent = root;
-			int child = parent * 2 + 1;
-			while (child < _con.size())
-			{
-				if (child + 1 < _con.size() && com(_con[child], _con[child + 1]))
-						++child;
-				if (com(_con[parent], _con[child]))
-				{
-						swap(_con[parent], _con[child]);
-						parent = child;
-						child = parent * 2 + 1;
-				}
-				else
-				{
-						break;
-				}
-			}
-		}
-
-	
 		void push(const T& x)
 		{
 			_con.push_back(x);
 			adjust_up(_con.size() - 1);
+		}
+
+		//向下调整大根堆
+		void adjust_down(int parent)
+		{
+			Compare com;
+			int child = parent * 2 + 1;
+			while (child < _con.size())
+			{
+				if (child + 1< _con.size() && com(_con[child], _con[child + 1]))
+					++child;
+				if (com(_con[parent],  _con[child]))
+				{
+					swap(_con[child], _con[parent]);
+					parent = child;
+					child = parent * 2 + 1;
+				}
+				else
+				{
+					break;
+				}
+			}
 		}
 		void pop()
 		{
@@ -83,44 +84,82 @@ namespace sheena
 			adjust_down(0);
 		}
 
-		size_t size()
-		{
-			return _con.size();
-		}
 		T& top()
 		{
 			return _con[0];
+		}
+		const T& top() const 
+		{
+			return _con[0];
+		}
+
+		size_t size()
+		{
+			return _con.size();
 		}
 		bool empty()
 		{
 			return _con.empty();
 		}
 	private:
-		Contanier _con;
+		Container _con;
 	};
 }
 
-using namespace sheena;
-
-int main()
+void testPriorityQueue1()
 {
-	sheena::priority_queue<int, vector<int>, sheena::my_less<int>> pq;
+	//默认为大的优先级高
+	sheena::my_priority_queue<int, vector<int>> pq;
+	pq.push(1);
+	pq.push(5);
+	pq.push(4);
 	pq.push(6);
-	pq.push(2);
 	pq.push(3);
-	pq.push(9);
-	pq.push(8);
-	pq.push(0);
-	pq.push(7);
-	cout << pq.size() << endl;
+	pq.push(2);
+
+	cout << "pq的size()为：" << pq.size() << endl;
+	cout << "pq的top()堆顶元素为：" << pq.top() << endl;
 
 	while (!pq.empty())
 	{
-		cout << pq.top() << "  ";
+		cout << pq.top() << endl;
 		pq.pop();
 	}
-	cout << endl;
-	cout << pq.size() << endl;
-	return 0;
 
+}
+void testPriorityQueue2()
+{
+	sheena::my_priority_queue<int, deque<int>, sheena::my_greater<int>> pq;
+	pq.push(1);
+	pq.push(5);
+	pq.push(4);
+	pq.push(6);
+	pq.push(3);
+	pq.push(2);
+
+	cout << "pq的size()为：" << pq.size() << endl;
+	cout << "pq的top()堆顶元素为：" << pq.top() << endl;
+
+	while (!pq.empty())
+	{
+		cout << pq.top() << endl;
+		pq.pop();
+	}
+
+}
+
+
+int main()
+{
+
+	cout << "testPriorityQueue1()默认为大的优先级高：" << endl;
+	testPriorityQueue1();
+	cout << endl;
+
+	cout << "testPriorityQueue2()变为小的优先级高：  " << endl;
+	testPriorityQueue2();
+	cout << endl;
+
+
+	return 0;
 }
